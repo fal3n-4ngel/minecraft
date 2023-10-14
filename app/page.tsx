f7d002b2b81115eaa7aedbfbd9a87c484b777489
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2023-10-14 23:15:08 +0530

    fix : overflow hide

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ServerStatus from './components/ServerStatus'
 
 export default function Home() {
   return (
-    <main className="flex flex-col bg-[#1a1a1a] min-h-screen items-center justify-center">
+    <main className="flex flex-col bg-[#1a1a1a] min-h-screen items-center justify-center overflow-hidden">
       <div className='flex items-center justify-center w-full md:text-[90px] text-2xl md:p-10'>✨🐾 Waifu No Weebs 🐾✨</div>
       <div className='flex items-center justify-center w-full md:text-[60px] text-xl md:p-5'>Minecraft Server</div>
 
